5e26167f34b943750900bcb8ed168ef6ca39d97d
bargavsiripurapu <bhargav.siripurapu@intuily.com>
2024-06-18 17:42:24 +0530

    Dynamodb code added in provider.tf & dynamo.tf

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -7,9 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-remote-backend-s3"
+    bucket = "terraform-remote-backend-s3-githubactions"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
   }
 }
 
