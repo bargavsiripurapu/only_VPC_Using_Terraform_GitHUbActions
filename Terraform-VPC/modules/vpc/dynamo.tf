1d9a9092ed160bd14d1287cf347067bf950ea1c6
Bhargav Siripurapu <104213005+bargavsiripurapu@users.noreply.github.com>
2024-06-18 18:10:16 +0530

    Update dynamo.tf

--- Terraform-VPC/modules/vpc/dynamo.tf
+++ Terraform-VPC/modules/vpc/dynamo.tf
@@ -1,13 +1,1 @@
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
 
-  hash_key = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
