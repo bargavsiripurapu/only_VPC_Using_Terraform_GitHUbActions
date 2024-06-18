1b2a686860e44df229efec182be9ae29170ade8b
Bhargav Siripurapu <104213005+bargavsiripurapu@users.noreply.github.com>
2024-06-18 20:28:16 +0530

    Update backend.tf

--- Terraform-VPC/backend.tf
+++ Terraform-VPC/backend.tf
@@ -1,9 +1,10 @@
 resource "aws_dynamodb_table" "state_locking" {
   hash_key = "LockID"
   name     = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
   attribute {
     name = "LockID"
     type = "S"
   }
-  billing_mode = "PAY_PER_REQUEST"
+  
 }
