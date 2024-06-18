0badec349cb0b38236a1b2779fdb78534fb0973e
bargavsiripurapu <bhargav.siripurapu@intuily.com>
2024-06-18 18:03:36 +0530

    Code updated

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -1,19 +1,36 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.16.1"
     }
   }
 
   backend "s3" {
-    bucket = "terraform-remote-backend-s3-githubactions"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "terraform-remote-backend-s3-githubactions"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-lock-table"
+  }
 }
