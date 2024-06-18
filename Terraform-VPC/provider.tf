e0c09fbf0d6f6228a3ebc8746f682dfc33b9c3eb
Bhargav Siripurapu <104213005+bargavsiripurapu@users.noreply.github.com>
2024-06-18 19:18:52 +0530

    Update provider.tf

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -17,20 +17,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "terraform-lock-table"
-  }
-}
