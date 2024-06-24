e7c989b9e5a848c1ee26cf1a24029e9f40aa815b
bargavsiripurapu <bhargav.siripurapu@intuily.com>
2024-06-24 11:31:18 +0530

    Code updated with EC2, Loadbalancer, EBS & Autoscaling

---
+++ Terraform-VPC/modules/ec2/main.tf
@@ -0,0 +1,44 @@
+resource "aws_instance" "web" {
+  count = length(var.ec2_names)
+  ami           = data.aws_ami.amazon-2.id
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [var.sg_id]
+  subnet_id = var.subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  
+  ebs_block_device {
+    device_name           = "/dev/sdh" # or another appropriate device name
+    volume_size           = 20 # specify the size of the volume in GiB
+    volume_type           = "gp2" # specify the volume type (gp2, io1, io2, etc.)
+    delete_on_termination = true # specify whether the volume should be deleted on instance termination
+  }
+
+  tags = {
+    Name = var.ec2_names[count.index]
+  }
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "auto_scaling_group" {
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnets
+  target_group_arns   = [aws_lb_target_group.tg.arn]
+
+  launch_template {
+    id      = aws_launch_template.launch_template.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = var.ec2_names[0] # Assuming all instances will have the same name tag for simplicity
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ Terraform-VPC/modules/ec2/variables.tf
@@ -0,0 +1,33 @@
+variable "sg_id" {
+  description = "SG ID for EC2"
+  type = string
+}
+
+variable "subnets" {
+  description = "Subnets for EC2"
+  type = list(string)
+}
+
+variable "ec2_names" {
+    description = "EC2 names"
+    type = list(string)
+    default = ["WebServer1", "WebServer2"]
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+  default     = 5
+}
+
+variable "desired_capacity" {
+  description = "The desired number of EC2 Instances in the ASG"
+  type        = number
+  default     = 3
+}
