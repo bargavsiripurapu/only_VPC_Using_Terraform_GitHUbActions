cc5e1f43124b6fff3c02b605582081edeba138bf
Bhargav Siripurapu <104213005+bargavsiripurapu@users.noreply.github.com>
2024-06-24 14:37:44 +0530

    Update variables.tf

--- Terraform-VPC/modules/ec2/variables.tf
+++ Terraform-VPC/modules/ec2/variables.tf
@@ -32,7 +32,7 @@ variable "desired_capacity" {
   default     = 3
 }
 
-variable "aws_lb_target_group.tg.arn" {
+variable "target_group_arn_id" {
   description = "tg arn for EC2"
   type = string
 }
