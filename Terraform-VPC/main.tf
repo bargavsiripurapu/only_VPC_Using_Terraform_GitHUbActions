b3ba2576b2371b98f3b4a922088b72ee355496d1
bargavsiripurapu <bhargav.siripurapu@intuily.com>
2024-06-25 14:29:19 +0530

    Coded Added

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -13,7 +13,6 @@ module "ec2" {
   source = "./modules/ec2"
   sg_id = module.sg.sg_id
   subnets = module.vpc.subnet_ids
-  target_group_arn = module.alb.target_group_arn
 }
 
 module "alb" {
--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -1,16 +1,13 @@
-
-# ASG with Launch template
 resource "aws_instance" "web" {
   count = length(var.ec2_names)
   ami           = data.aws_ami.amazon-2.id
   instance_type = "t2.micro"
-  #user_data     = filebase64("user_data.sh")
   associate_public_ip_address = true
   vpc_security_group_ids = [var.sg_id]
   subnet_id = var.subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
   
-ebs_block_device {
+  ebs_block_device {
     device_name           = "/dev/sdh" # or another appropriate device name
     volume_size           = 20 # specify the size of the volume in GiB
     volume_type           = "gp2" # specify the volume type (gp2, io1, io2, etc.)
@@ -19,27 +16,5 @@ ebs_block_device {
 
   tags = {
     Name = var.ec2_names[count.index]
-  }
-}
-
-resource "aws_autoscaling_group" "aws_asg" {
-  # no of instances
-  desired_capacity = 3
-  max_size         = 5
-  min_size         = 2
-
-  # Connect to the target group
-  target_group_arns = [aws_lb_target_group.tg.arn]
-  vpc_zone_identifier  = var.subnets[count.index]
-
-  launch_template {
-    id      = aws_launch_template.web.id
-    version = "$Latest"
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "web-server"
-    propagate_at_launch = true
   }
 }
