041419d2ff9e8c27460194c88a373a75b6239467
bargavsiripurapu <bhargav.siripurapu@intuily.com>
2024-06-25 11:55:35 +0530

    Code Updated with Autoscaling

--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -1,13 +1,16 @@
+
+# ASG with Launch template
 resource "aws_instance" "web" {
   count = length(var.ec2_names)
   ami           = data.aws_ami.amazon-2.id
   instance_type = "t2.micro"
+  #user_data     = filebase64("user_data.sh")
   associate_public_ip_address = true
   vpc_security_group_ids = [var.sg_id]
   subnet_id = var.subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
   
-  ebs_block_device {
+ebs_block_device {
     device_name           = "/dev/sdh" # or another appropriate device name
     volume_size           = 20 # specify the size of the volume in GiB
     volume_type           = "gp2" # specify the volume type (gp2, io1, io2, etc.)
@@ -16,5 +19,22 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = var.ec2_names[count.index]
+  }
+}
+
+resource "aws_autoscaling_group" "aws_asg" {
+  # no of instances
+  desired_capacity = 3
+  max_size         = 2
+  min_size         = 5
+
+  # Connect to the target group
+  target_group_arns = [aws_lb_target_group.tg.arn]
+
+  vpc_zone_identifier = [aws_subnet.subnets[count.index].id]
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
   }
 }
